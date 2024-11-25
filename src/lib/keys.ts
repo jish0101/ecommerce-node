ae526337d5f16e93591a9c4192fae1f9a7a3d302
Hero <76834176+jish0101@users.noreply.github.com>
2024-11-25 14:13:22 +0530

    Updated: env config

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -1,9 +1,9 @@
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({
+dotenv.config(process.env.NODE_ENV === "development" ? {
   path: path.resolve(__dirname, "..", "..", ".env"),
-});
+}: undefined);
 
 export const KEYS: Record<string, string> = {
   PORT: process.env.PORT!,
